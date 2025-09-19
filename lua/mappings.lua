555314bd1c2a7567054942ca3c11531635c254cc
norad32 <fredy.hirt@hispeed.ch>
2025-09-19 07:34:20 +0200

    Add mappings to select all and move selections

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,14 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<C-a>", "ggVG", { noremap = true, silent = true, desc = "Select all" })
+
+map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+
+map("x", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("x", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
 map("n", "<F5>", "<cmd>DapContinue<CR>", { desc = "Start/Continue" })
 map("n", "<F10>", "<cmd>DapStepOver<CR>", { desc = "Step Over" })
 map("n", "<F11>", "<cmd>DapStepInto<CR>", { desc = "Step Into" })
